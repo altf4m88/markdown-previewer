c3412a2004df358f5f5a2047b4a91d0f03ab6735
altf4m88 <fadhilmauladhani@gmail.com>
2021-10-31 19:58:50 +0700

    create initial compiler

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import './Components/MarkdownPreviewer';
+import MarkdownPreviewer from './Components/MarkdownPreviewer';
 
 function App() {
   return (
-
+    <MarkdownPreviewer/>
   );
 }
 
--- src/Components/MarkdownPreviewer.jsx
+++ src/Components/MarkdownPreviewer.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
+import marked from 'marked';
 
 class MarkdownPreviewer extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            markdown : ''
+            markdown : '# Marked in browser\n\nRendered by **marked**.'
         }
+        this.initialMarkdown = this.state.markdown;
+    }
+
+    componentDidMount(){
+        let previewElement = document.querySelector('#preview');
+
+        previewElement.innerHTML = marked(this.state.markdown);
     }
 
     render(){
         return(
-            <div>Guten abend!</div>
+            <div className="main-div d-flex">
+                <div class="card mr-2 w-50">
+                    <div class="card-header"><h4>Markdown</h4></div>
+                    <div class="card-body p-0">
+                        <textarea id="editor" className="form-control h-100">{this.state.markdown}</textarea>
+                    </div>
+                </div>
+                <div class="card ml-2 w-50">
+                    <div class="card-header"><h4>Preview</h4></div>
+                    <div class="card-body">
+                        <div id="preview">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
